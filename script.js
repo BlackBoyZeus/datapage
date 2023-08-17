a6410fead521335adb5505d711975ec8b4cf3e52
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-17 14:32:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -68,9 +68,84 @@ function visualizeData() {
 }
 
 function updateRevenuePredictionVisualization(data) {
-    // Use D3.js to update the revenue prediction visualization with new data
-    // This will depend on the structure of 'data' and the specifics of the visualization
-    // ...
-}
+    const svg = d3.select("#visualization");
+    const width = 800;
+    const height = 500;
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 };
 
-// Add more functions for preprocessing, visualization, and other analyses as needed
+    // Combine historical and predicted data
+    const combinedData = df.concat(data);
+
+    const x = d3.scaleBand()
+                .domain(combinedData.map(d => d['Event Breakdown']))
+                .range([margin.left, width - margin.right])
+                .padding(0.1);
+
+    const y = d3.scaleLinear()
+                .domain([0, d3.max(combinedData, d => d['Total Revenue'])])
+                .range([height - margin.bottom, margin.top]);
+
+    // Clear previous visualizations
+    svg.selectAll("*").remove();
+
+    // Create bars
+    svg.selectAll('rect')
+       .data(combinedData)
+       .enter()
+       .append('rect')
+       .attr('x', d => x(d['Event Breakdown']))
+       .attr('y', d => y(d['Total Revenue']))
+       .attr('width', x.bandwidth())
+       .attr('height', d => height - margin.bottom - y(d['Total Revenue']))
+       .attr('fill', d => 'predicted' in d ? 'orange' : 'blue') // Color predicted bars differently
+       .on('mouseover', function(d) {
+           // Display revenue details on hover
+           const tooltip = svg.append('text')
+                              .text(`Event: ${d['Event Breakdown']}, Revenue: ${d['Total Revenue']}`)
+                              .attr('x', d => x(d['Event Breakdown']))
+                              .attr('y', d => y(d['Total Revenue']) - 10)
+                              .attr('font-size', '12px')
+                              .attr('font-weight', 'bold')
+                              .attr('fill', 'black');
+       })
+       .on('mouseout', function(d) {
+           // Remove tooltip on mouseout
+           svg.selectAll('text').remove();
+       });
+
+    // Axes
+    svg.append('g')
+       .attr('transform', `translate(0, ${height - margin.bottom})`)
+       .call(d3.axisBottom(x));
+
+    svg.append('g')
+       .attr('transform', `translate(${margin.left}, 0)`)
+       .call(d3.axisLeft(y));
+
+    // Legends
+    const legend = svg.append('g')
+                      .attr('transform', `translate(${width - margin.right - 150}, ${margin.top})`);
+
+    legend.append('rect')
+          .attr('width', 20)
+          .attr('height', 20)
+          .attr('fill', 'blue');
+
+    legend.append('text')
+          .text('Historical Data')
+          .attr('x', 30)
+          .attr('y', 15)
+          .attr('font-size', '12px');
+
+    legend.append('rect')
+          .attr('width', 20)
+          .attr('height', 20)
+          .attr('fill', 'orange')
+          .attr('y', 25);
+
+    legend.append('text')
+          .text('Predicted Revenue')
+          .attr('x', 30)
+          .attr('y', 40)
+          .attr('font-size', '12px');
+}
