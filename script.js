8217cbb90f823219525499353a50b5a864eea5ab
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-17 17:23:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,174 @@
-let currentData = []; // This will hold the data from the CSV
+// Global variable for data storage
+let data;
 
-// Read the uploaded CSV
-function readFile(input) {
-    console.log("Reading file...");
-    let file = input.files[0];
-    d3.csv(file).then(data => {
-        console.log("File read successfully.");
-        preprocessData(data);
+async function processFile() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        updateStatus("No file selected.", "error");
+        return;
+    }
+
+    try {
+        updateStatus("Reading file...");
+        const fileContent = await readFile(file);
+        
+        updateStatus("Processing CSV...");
+        data = csvToJSON(fileContent);
+        if (!data || data.length === 0) {
+            updateStatus("Error processing the CSV file.", "error");
+            return;
+        }
+
+        updateStatus("Visualizing data...");
+        displayBarChart();
+        displayPieChart();
+        displayAggregateMetrics();
+
+        updateStatus("Visualization complete!", "success");
+    } catch (error) {
+        updateStatus("An error occurred: " + error.message, "error");
+    }
+}
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = reject;
+        reader.readAsText(file);
     });
 }
 
-// Convert the data into a format suitable for clustering
-function preprocessData(data) {
-    console.log("Preprocessing data...");
-    currentData = data.map(d => [parseFloat(d.x), parseFloat(d.y)]); 
-    console.log("Data preprocessed.");
+function updateStatus(message, type = "info") {
+    const statusDiv = document.getElementById('status');
+    statusDiv.textContent = message;
+    statusDiv.className = type;
 }
 
-// Start clustering and visualization
-function startClustering() {
-    console.log("Starting clustering...");
-    let clusters = kMeans(currentData, 3); 
-    console.log("Clustering completed.");
-    visualizeData(clusters);
-}
+function csvToJSON(csv) {
+    const lines = csv.trim().split('\n');
+    const result = [];
+    const headers = lines[0].split(',');
 
-// K-means clustering
-function kMeans(data, k = 3) {
-    // Initialize k centroids randomly
-    let centroids = [];
-    for (let i = 0; i < k; i++) {
-        centroids.push(data[Math.floor(Math.random() * data.length)]);
+    for (let i = 1; i < lines.length; i++) {
+        const obj = {};
+        const currentLine = lines[i].split(',');
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentLine[j];
+        }
+        result.push(obj);
     }
-
-    let previousCentroids;
-    let iterations = 0;
-    while (!isEqual(centroids, previousCentroids) && iterations < 10) {
-        iterations++;
-        console.log(`Iteration ${iterations}...`);
-
-        // Assign each data point to the nearest centroid
-        let clusters = assignDataToCentroids(data, centroids);
-
-        // Recalculate centroids
-        previousCentroids = centroids.slice();
-        centroids = recalculateCentroids(clusters);
-    }
-
-    return clusters;
+    return result;
 }
 
-function assignDataToCentroids(data, centroids) {
-    return data.reduce((clusters, point) => {
-        let closestCentroidIdx = centroids.map(centroid => 
-            euclideanDistance(point, centroid)).indexOf(Math.min(...centroids.map(centroid => 
-                euclideanDistance(point, centroid))));
-        clusters[closestCentroidIdx].push(point);
-        return clusters;
-    }, Array.from({ length: centroids.length }, () => []));
-}
+function displayBarChart() {
+    const ctx = document.getElementById('visualization').getContext('2d');
+    const labels = data.map(row => row['Sponsors'] || 'Unknown');
+    const values = data.map(row => parseFloat(row['Event Breakdown'] || 0));
 
-function recalculateCentroids(clusters) {
-    return clusters.map(cluster => {
-        let sumX = 0, sumY = 0;
-        cluster.forEach(point => {
-            sumX += point[0];
-            sumY += point[1];
-        });
-        return [sumX / cluster.length, sumY / cluster.length];
-    });
-}
-
-// Euclidean distance between two points
-function euclideanDistance(pointA, pointB) {
-    return Math.sqrt(Math.pow(pointA[0] - pointB[0], 2) + Math.pow(pointA[1] - pointB[1], 2));
-}
-
-// Check if two sets of points are equal
-function isEqual(pointsA, pointsB) {
-    if (!pointsA || !pointsB) return false;
-    if (pointsA.length !== pointsB.length) return false;
-    for (let i = 0; i < pointsA.length; i++) {
-        if (pointsA[i][0] !== pointsB[i][0] || pointsA[i][1] !== pointsB[i][1]) return false;
-    }
-    return true;
-}
-
-// Visualize the data using Chart.js
-function visualizeData(clusters) {
-    console.log("Visualizing data...");
-    let ctx = document.getElementById('visualization').getContext('2d');
     new Chart(ctx, {
-        type: 'scatter',
+        type: 'bar',
         data: {
-            datasets: clusters.map((cluster, idx) => {
-                return {
-                    label: 'Cluster ' + idx,
-                    data: cluster.map(point => ({ x: point[0], y: point[1] })),
-                    borderColor: getRandomColor(),
-                    backgroundColor: 'transparent'
-                };
-            })
+            labels: labels,
+            datasets: [{
+                label: 'Event Breakdown ($)',
+                data: values,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
         },
         options: {
+            responsive: true,
             scales: {
-                xAxes: [{
-                    type: 'linear',
-                    position: 'bottom'
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
+            },
+            plugins: {
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'xy'
+                    },
+                    zoom: {
+                        enabled: true,
+                        mode: 'xy'
+                    }
+                }
             }
         }
     });
-    console.log("Visualization completed.");
 }
 
-// Get a random color
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function displayPieChart() {
+    const ctxMetrics = document.getElementById('metrics').getContext('2d');
+    const venues = [...new Set(data.map(row => row['Venue']))];
+    const venueCounts = venues.map(venue => data.filter(row => row['Venue'] === venue).length);
+
+    new Chart(ctxMetrics, {
+        type: 'pie',
+        data: {
+            labels: venues,
+            datasets: [{
+                data: venueCounts,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
 }
+
+function displayAggregateMetrics() {
+    const totalRevenue = data.reduce((acc, row) => acc + parseFloat(row['Total Revenue'] || 0), 0);
+    const averageEventBreakdown = data.reduce((acc, row) => acc + parseFloat(row['Event Breakdown'] || 0), 0) / data.length;
+
+    document.getElementById('aggregateMetrics').innerHTML = `
+        <strong>Total Revenue:</strong> $${totalRevenue.toFixed(2)}<br>
+        <strong>Average Event Breakdown:</strong> $${averageEventBreakdown.toFixed(2)}
+    `;
+}
+
+// D3.js Tooltip for extra information on hover
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+document.getElementById('visualization').onmousemove = function(event) {
+    const index = Math.floor(event.offsetX / 100);
+    if (data[index]) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html(`
+            Sponsors: ${data[index]['Sponsors']}<br>
+            Venue: ${data[index]['Venue']}<br>
+            Total Revenue: ${data[index]['Total Revenue']}
+        `)
+        .style("left", (event.pageX + 5) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    }
+};
+
+document.getElementById('visualization').onmouseout = function() {
+    tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+};
