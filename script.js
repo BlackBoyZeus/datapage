60c7cc6e34b08a1b7db1759d324f1677c008df8f
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-17 14:30:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,174 +1,76 @@
-importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs');
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.4.3/math.min.js');
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/simple-statistics/7.5.0/simple-statistics.min.js');
+let df;  // This will hold our DataFrame
 
-onmessage = function(e) {
-    const { type, data } = e.data;
+// Web Worker setup for offloading heavy computations
+const worker = new Worker('analysisWorker.js');
 
-    switch(type) {
-        case 'processWithTensorFlow':
-            postMessage({ type: 'tensorflowData', data: processDataWithTensorFlow(data) });
+// Listen for messages from the worker
+worker.onmessage = function(event) {
+    const { type, data } = event.data;
+
+    // Handle messages by type
+    switch (type) {
+        case 'revenuePrediction':
+            updateRevenuePredictionVisualization(data);
             break;
-        case 'calculateMean':
-            postMessage({ type: 'meanValue', data: calculateMeanWithValue(data) });
-            break;
-        case 'calculateCorrelation':
-            postMessage({ type: 'correlationCoefficient', data: calculateCorrelationCoefficient(data) });
-            break;
-        default:
-            console.error("Unknown job type", type);
+        // Handle other message types as needed
+        // ...
     }
 };
 
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-const worker = new Worker('dataWorker.js');
-worker.onmessage = function(e) {
-    const { type, data } = e.data;
-
-    switch(type) {
-        case 'tensorflowData':
-            // Use the processed data for visualizations or further processing
-            createD3BarChart(data);
-            createChartJSLineChart(data);
-            displayDataWithAgGrid(data);
-            break;
-        case 'meanValue':
-            // Use the calculated mean value
-            displayMessage(`Mean of value1: ${data}`);
-            break;
-        case 'correlationCoefficient':
-            // Use the correlation coefficient
-            displayMessage(`Correlation Coefficient between value1 and value2: ${data}`);
-            break;
-        default:
-            console.error("Unknown result type", type);
-    }
-};
-
-document.addEventListener("DOMContentLoaded", function() {
-    const fileInput = document.getElementById("fileInput");
-    const uploadButton = document.getElementById("uploadButton");
-    const messageContainer = document.getElementById("messageContainer");
-
-    function displayMessage(message, isError = false) {
-        const messageElement = document.createElement("p");
-        messageElement.textContent = message;
-        if (isError) {
-            messageElement.style.color = "red";
-        }
-        messageContainer.appendChild(messageElement);
-    }
-
-    uploadButton.addEventListener("click", function() {
-    const file = fileInput.files[0];
-    if (!file) {
-        displayMessage("Error: Please select a file first.", true);
-        return;
-    }
-
-    Papa.parse(file, {
-        complete: function(results) {
-            if (results.errors.length > 0) {
-                displayMessage(`Error parsing CSV: ${results.errors[0].message}`, true);
-                return;
-            }
-
-            const data = results.data;
-            worker.postMessage({ type: 'processWithTensorFlow', data: data });
-            worker.postMessage({ type: 'calculateMean', data: data });
-            worker.postMessage({ type: 'calculateCorrelation', data: data });
-        },
-        header: true,
-        skipEmptyLines: true
-    });
-});
-
-                const data = results.data;
-                const processedData = processDataWithTensorFlow(data);
-
-                createD3BarChart(processedData);
-                createChartJSLineChart(processedData);
-                displayDataWithAgGrid(processedData);
-                
-                const filteredData = filterDataWithLodash(processedData);
-                const dateFormattedData = formatDatesWithDataFns(filteredData);
-                
-                const meanValue = calculateMeanWithValue(dateFormattedData);
-                const correlationCoefficient = calculateCorrelationCoefficient(dateFormattedData);
-
-                displayMessage(`Mean of value1: ${meanValue}`);
-                displayMessage(`Correlation Coefficient between value1 and value2: ${correlationCoefficient}`);
-
-                // Hide the loader when processing finishes
-                document.getElementById("loader-section").style.display = "none";
-            },
-            header: true,
-            skipEmptyLines: true
-        });
-    });
-
-    function processDataWithTensorFlow(data) {
-        displayMessage("Processing data with TensorFlow.js...");
-        const values1 = data.map(row => parseFloat(row.value1));
-        const tensor1 = tf.tensor(values1);
-
-        const normalizedTensor1 = tensor1.sub(tensor1.min()).div(tensor1.max().sub(tensor1.min()));
-        const normalizedValues1 = normalizedTensor1.arraySync();
-
-        return data.map((row, index) => ({
-            ...row,
-            normalizedValue1: normalizedValues1[index]
-        }));
-    }
-
-    function createD3BarChart(data) {
-        displayMessage("Generating D3.js bar chart...");
-        const svg = d3.select("#d3js-container").append("svg").attr("width", 500).attr("height", 500);
-        svg.selectAll("rect")
-           .data(data)
-           .enter().append("rect")
-           .attr("x", (d, i) => i * 45)
-           .attr("y", d => 500 - d.value1 * 10)
-           .attr("width", 40)
-           .attr("height", d => d.value1 * 10)
-           .attr("fill", "#FF5733");
-    }
-
-    function createChartJSLineChart(data) {
-        displayMessage("Generating Chart.js line chart...");
-        const ctx = document.getElementById("chartjs-container").getContext("2d");
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: data.map(row => row.date),
-                datasets: [{
-                    label: 'Value 1',
-                    data: data.map(row => row.value1),
-                    borderColor: "#FF5733",
-                    fill: false
-                }]
-            }
-        });
-    }
-
-    function displayDataWithAgGrid(data) {
-        displayMessage("Displaying data using Ag-Grid...");
-        const gridOptions = {
-            columnDefs: Object.keys(data[0]).map(key => ({headerName: key, field: key})),
-            rowData: data
-        };
+    reader.onload = function(e) {
+        const data = e.target.result;
+        df = new pd.DataFrame(data);
         
-        const gridDiv = document.querySelector("#ag-grid-container");
-        new agGrid.Grid(gridDiv, gridOptions);
+        // Start the initial analysis
+        analyzeData();
     }
 
-    function calculateStatistics(data) {
-        const meanValue1 = math.mean(data.map(row => parseFloat(row.value1)));
-        const stdDevValue1 = simpleStats.standardDeviation(data.map(row => parseFloat(row.value1)));
-        const correlation = simpleStats.sampleCorrelation(data.map(row => parseFloat(row.value1)), data.map(row => parseFloat(row.value2)));
-
-        displayMessage(`Mean of value1: ${meanValue1}`);
-        displayMessage(`Standard Deviation of value1: ${stdDevValue1}`);
-        displayMessage(`Correlation between value1 and value2: ${correlation}`);
-    }
+    reader.readAsText(file);
 });
+
+function analyzeData() {
+    preprocessData();
+    visualizeData();
+
+    // Send data to worker for heavy computations
+    worker.postMessage({
+        type: 'startAnalysis',
+        data: df // Note: In a real-world scenario, you'd need to serialize the DataFrame or send raw data
+    });
+}
+
+function preprocessData() {
+    // Example: Convert 'Event Breakdown' to integers
+    df['Event Breakdown'] = df['Event Breakdown'].str.replace(',', '').astype('int');
+
+    // Handle missing values, for example by filling with defaults or removing rows
+    df = df.dropna();
+}
+
+function visualizeData() {
+    // Use D3.js to create initial visualizations
+    // Example: Create a bar chart of revenue by event
+    const svg = d3.select("#visualization");
+    const x = d3.scaleBand().domain(df['Event Breakdown']).range([0, 500]);
+    const y = d3.scaleLinear().domain([0, d3.max(df['Total Revenue'])]).range([500, 0]);
+
+    svg.selectAll('rect')
+       .data(df)
+       .enter().append('rect')
+       .attr('x', d => x(d['Event Breakdown']))
+       .attr('y', d => y(d['Total Revenue']))
+       .attr('width', x.bandwidth())
+       .attr('height', d => 500 - y(d['Total Revenue']));
+}
+
+function updateRevenuePredictionVisualization(data) {
+    // Use D3.js to update the revenue prediction visualization with new data
+    // This will depend on the structure of 'data' and the specifics of the visualization
+    // ...
+}
+
+// Add more functions for preprocessing, visualization, and other analyses as needed
