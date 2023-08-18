d789a1cf2e3c468ada26cb68e31dd86c595812e0
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-18 16:43:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Global variable for data storage
+let data;
+
 async function processFile() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
+
     if (!file) {
         updateStatus("No file selected.", "error");
         return;
     }
+
     try {
         updateStatus("Reading file...");
         const fileContent = await readFile(file);
@@ -15,52 +20,61 @@ async function processFile() {
             updateStatus("Error processing the CSV file.", "error");
             return;
         }
+
         updateStatus("Visualizing data...");
         displayBarChart();
-        trainAndVisualize();
+        displayPieChart();
+        displayAggregateMetrics();
+
         updateStatus("Visualization complete!", "success");
     } catch (error) {
         updateStatus("An error occurred: " + error.message, "error");
-document.getElementById('visualization').onmouseout = function() {
-};
+    }
+}
 
-
-function trainAndVisualize() {
-    // 1. Data extraction and cleaning
-    const totalRevenues = data.map(row => {
-        let revenue = row['Total Revenue'];
-        if (!revenue) return 0;
-        if (revenue.includes('M+')) return parseFloat(revenue.replace('M+', '').replace('$', '')) * 1000000;
-        return parseFloat(revenue.replace('$', '').replace(',', '')) || 0;
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = reject;
+        reader.readAsText(file);
     });
-async function trainAndVisualize() {
-    try {
-        updateStatus("Initializing...");
+}
 
-    const sponsors = data.map(row => row['Sponsors'] || 'Unknown');
-    const venues = [...new Set(data.map(row => row['Venue']))];
-    const venueCounts = venues.map(venue => data.filter(row => row['Venue'] === venue).length);
-        // Extracting and cleaning 'Total Revenue' data
-        const totalRevenues = data.map(row => {
-        const totalRevenue = data.map(row => {
-            let revenue = row['Total Revenue'];
-            if (!revenue) return 0;
-            if (revenue.includes('M+')) return parseFloat(revenue.replace('M+', '').replace('$', '')) * 1000000;
-            return parseFloat(revenue.replace('$', '').replace(',', '')) || 0;
-        });
+function updateStatus(message, type = "info") {
+    const statusDiv = document.getElementById('status');
+    statusDiv.textContent = message;
+    statusDiv.className = type;
+}
 
-    // 2. Bar chart: Total Revenue vs Sponsors
-    const ctxBar = document.getElementById('barChart').getContext('2d');
-        // Convert data into tensor
-        const tensorData = tf.tensor2d(totalRevenues, [totalRevenues.length, 1]);
+function csvToJSON(csv) {
+    const lines = csv.trim().split('\n');
+    const result = [];
+    const headers = lines[0].split(',');
 
-    new Chart(ctxBar, {
+    for (let i = 1; i < lines.length; i++) {
+        const obj = {};
+        const currentLine = lines[i].split(',');
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentLine[j];
+        }
+        result.push(obj);
+    }
+    return result;
+}
+
+function displayBarChart() {
+    const ctx = document.getElementById('visualization').getContext('2d');
+    const labels = data.map(row => row['Sponsors'] || 'Unknown');
+    const values = data.map(row => parseFloat(row['Event Breakdown'] || 0));
+
+    new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: sponsors,
+            labels: labels,
             datasets: [{
-                label: 'Total Revenue ($)',
-                data: totalRevenues,
+                label: 'Event Breakdown ($)',
+                data: values,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
@@ -73,101 +87,30 @@ async function trainAndVisualize() {
                     ticks: {
                         beginAtZero: true
                     }
-        // Define a simple model
-        const model = tf.sequential();
-        model.add(tf.layers.dense({ units: 3, inputShape: [1], activation: 'softmax' }));
-        model.compile({ loss: 'sparseCategoricalCrossentropy', optimizer: tf.train.adam(), metrics: ['accuracy'] });
-
-        // Generate labels for clustering
-        const maxRevenue = Math.max(...totalRevenues);
-        const minRevenue = Math.min(...totalRevenues);
-        const midRevenue = (maxRevenue + minRevenue) / 2;
-
-        const labels = totalRevenues.map(rev => {
-            if (rev <= midRevenue) return 0; // Low
-            else if (rev > midRevenue && rev <= (midRevenue + maxRevenue) / 2) return 1; // Medium
-            else return 2; // High
-        });
-
-        const tensorLabels = tf.tensor2d(labels, [labels.length, 1]);
-        // Check for NaN values in the extracted data
-        if (totalRevenue.includes(NaN)) {
-            updateStatus("Invalid data detected in 'Total Revenue'. Ensure all values are numeric.", "error");
-            return;
-        }
-
-        updateStatus("Training the model...");
-        await model.fit(tensorData, tensorLabels, {
-            epochs: 10,
-            shuffle: true
-        });
-        // Calculate average growth in revenue
-        let growths = [];
-        for (let i = 1; i < totalRevenue.length; i++) {
-            growths.push((totalRevenue[i] - totalRevenue[i - 1]) / totalRevenue[i - 1]);
-        }
-        const avgGrowth = growths.reduce((acc, val) => acc + val, 0) / growths.length;
-
-        const predictions = model.predict(tensorData).argMax(-1).dataSync();
-        // Forecast future revenue based on historical data and average growth
-        const forecastData = totalRevenue.map(revenue => revenue * (1 + avgGrowth));
-
-        updateStatus("Visualizing the data...");
-
-        // Visualizing the clusters
-        const colors = predictions.map(pred => {
-            if (pred === 0) return 'rgba(75, 192, 192, 0.2)'; // Low
-            if (pred === 1) return 'rgba(255, 206, 86, 0.2)'; // Medium
-            if (pred === 2) return 'rgba(255, 99, 132, 0.2)'; // High
-        });
-
-        const borderColors = predictions.map(pred => {
-            if (pred === 0) return 'rgba(75, 192, 192, 1)';
-            if (pred === 1) return 'rgba(255, 206, 86, 1)';
-            if (pred === 2) return 'rgba(255, 99, 132, 1)';
-        });
-        const ctxForecast = document.getElementById('forecast').getContext('2d');
-        const labels = Array.from({ length: totalRevenue.length }, (_, i) => `Event ${i + 1}`);
-
-        const ctx = document.getElementById('visualization').getContext('2d');
-        new Chart(ctx, {
-            type: 'bar',
-        new Chart(ctxForecast, {
-            type: 'line',
-            data: {
-                labels: data.map((_, index) => 'Event ' + (index + 1)),
-                labels: labels,
-                datasets: [{
-                    label: 'Total Revenue ($)',
-                    data: totalRevenues,
-                    backgroundColor: colors,
-                    borderColor: borderColors,
-                    label: 'Historical Revenue ($)',
-                    data: totalRevenue,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Forecasted Revenue ($)',
-                    data: forecastData,
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
                 }]
             },
-async function trainAndVisualize() {
-                    }]
+            plugins: {
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'xy'
+                    },
+                    zoom: {
+                        enabled: true,
+                        mode: 'xy'
+                    }
                 }
             }
         }
     });
+}
 
-    // 3. Pie chart: Events per Venue
-    const ctxPie = document.getElementById('pieChart').getContext('2d');
-        });
+function displayPieChart() {
+    const ctxMetrics = document.getElementById('metrics').getContext('2d');
+    const venues = [...new Set(data.map(row => row['Venue']))];
+    const venueCounts = venues.map(venue => data.filter(row => row['Venue'] === venue).length);
 
-    new Chart(ctxPie, {
+    new Chart(ctxMetrics, {
         type: 'pie',
         data: {
             labels: venues,
@@ -191,26 +134,41 @@ async function trainAndVisualize() {
             }]
         }
     });
-        updateStatus("Visualization complete!", "success");
-    } catch (error) {
-        updateStatus("An error occurred: " + error.message, "error");
+}
+
+function displayAggregateMetrics() {
+    const totalRevenue = data.reduce((acc, row) => acc + parseFloat(row['Total Revenue'] || 0), 0);
+    const averageEventBreakdown = data.reduce((acc, row) => acc + parseFloat(row['Event Breakdown'] || 0), 0) / data.length;
+
+    document.getElementById('aggregateMetrics').innerHTML = `
+        <strong>Total Revenue:</strong> $${totalRevenue.toFixed(2)}<br>
+        <strong>Average Event Breakdown:</strong> $${averageEventBreakdown.toFixed(2)}
+    `;
+}
+
+// D3.js Tooltip for extra information on hover
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+document.getElementById('visualization').onmousemove = function(event) {
+    const index = Math.floor(event.offsetX / 100);
+    if (data[index]) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html(`
+            Sponsors: ${data[index]['Sponsors']}<br>
+            Venue: ${data[index]['Venue']}<br>
+            Total Revenue: ${data[index]['Total Revenue']}
+        `)
+        .style("left", (event.pageX + 5) + "px")
+        .style("top", (event.pageY - 28) + "px");
     }
-}
+};
 
-
-
-
-
-
-// This function remains unchanged
-function updateStatus(message, type = "info") {
-    const statusDiv = document.getElementById('status');
-    statusDiv.textContent = message;
-    statusDiv.className = type;
-}
-
-// New function to handle the button click
-async function onClusterDataClick() {
-    await processFile();
-    trainAndVisualize();
-}
+document.getElementById('visualization').onmouseout = function() {
+    tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+};
