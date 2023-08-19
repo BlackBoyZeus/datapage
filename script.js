a35f8c5559e4917203e57d6d9e7521c0a0dc1793
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-19 11:31:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,42 @@
-// Global variable for data storage
-let data;
+// Global variable to store the data from the uploaded CSV
+let data = [];
 
-async function processFile() {
+// Function to process the uploaded file and visualize it
+function processFile() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
-    if (!file) {
-        updateStatus("No file selected.", "error");
-        return;
-    }
-    try {
-        updateStatus("Reading file...");
-        const fileContent = await readFile(file);
-        
-        updateStatus("Processing CSV...");
-        data = csvToJSON(fileContent);
-        if (!data || data.length === 0) {
-            updateStatus("Error processing the CSV file.", "error");
-            return;
-        }
-        updateStatus("Visualizing data...");
-        displayBarChart();
-        displayPieChart();
-        displayAggregateMetrics();
-        updateStatus("Visualization complete!", "success");
-    } catch (error) {
-        updateStatus("An error occurred: " + error.message, "error");
-    }
-}
+    const reader = new FileReader();
 
-function readFile(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = event => resolve(event.target.result);
-        reader.onerror = reject;
-        reader.readAsText(file);
-    });
-}
-
-function updateStatus(message, type = "info") {
-    const statusDiv = document.getElementById('status');
-    statusDiv.textContent = message;
-    statusDiv.className = type;
-}
-
-function csvToJSON(csv) {
-    const result = [];
-    const rows = csv.trim().split('\n');
-    
-    // Extract headers from the first row
-    const headers = rows[0].split(',').map(header => header.trim());
-
-    for (let i = 1; i < rows.length; i++) {
-        const row = rows[i];
-        const values = [];
-        
-        // Use regex to split by commas not within double quotes
-        const regex = /,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/;
-        const splitValues = row.split(regex);
-        
-        splitValues.forEach(value => {
-            // Remove double quotes and trim spaces
-            values.push(value.replace(/^"|"$/g, '').trim());
+    reader.onload = function(event) {
+        const csv = event.target.result;
+        d3.csvParse(csv, function(d) {
+            // Assuming the CSV has columns 'label' and 'value' for simplicity
+            return {
+                label: d.label,
+                value: +d.value // Convert the value to a number
+            };
+        }).then(function(parsedData) {
+            data = parsedData;
+            visualizeData();
         });
-        
-        if (values.length !== headers.length) {
-            // Skip rows with inconsistent number of values
-            continue;
-        }
-        
-        const obj = {};
-        for (let j = 0; j < headers.length; j++) {
-            const value = values[j];
-            obj[headers[j]] = headers[j] === 'Event Breakdown' || headers[j] === 'Total Revenue' 
-                              ? parseFloat(value.replace(/,/g, '')) 
-                              : value;
-        }
-        
-        // Only add rows where key columns have values
-        if (obj['Sponsors'] && obj['Event Breakdown'] && obj['Venue']) {
-            result.push(obj);
-        }
-    }
-    return result;
+    };
+
+    reader.readAsText(file);
 }
 
-
-function displayBarChart() {
+// Function to visualize the uploaded data using a bar chart
+function visualizeData() {
     const ctx = document.getElementById('visualization').getContext('2d');
-    const labels = data.map(row => row['Sponsors'] || 'Unknown');
-    const values = data.map(row => row['Event Breakdown'] || 0);
+
+    const labels = data.map(d => d.label);
+    const values = data.map(d => d.value);
+
     new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
             datasets: [{
-                label: 'Event Breakdown ($)',
+                label: 'Data',
                 data: values,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
@@ -102,93 +44,43 @@ function displayBarChart() {
             }]
         },
         options: {
-            responsive: true,
             scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            },
-            plugins: {
-                zoom: {
-                    pan: {
-                        enabled: true,
-                        mode: 'xy'
-                    },
-                    zoom: {
-                        enabled: true,
-                        mode: 'xy'
-                    }
+                y: {
+                    beginAtZero: true
                 }
             }
         }
     });
 }
 
-function displayPieChart() {
-    const ctxMetrics = document.getElementById('metrics').getContext('2d');
-    const venues = [...new Set(data.map(row => row['Venue']))].filter(Boolean);
-    const venueCounts = venues.map(venue => data.filter(row => row['Venue'] === venue).length);
-    new Chart(ctxMetrics, {
-        type: 'pie',
+// Function to run earnings forecast simulation and visualize it
+function earnings_forecast_simulation_millions() {
+    const ctx = document.getElementById('metrics').getContext('2d');
+
+    // Generate random forecasted earnings for simplicity
+    const labels = [...Array(12).keys()].map(d => `Month ${d+1}`);
+    const values = labels.map(() => Math.random() * 1000); // Random earnings in millions
+
+    new Chart(ctx, {
+        type: 'line',
         data: {
-            labels: venues,
+            labels: labels,
             datasets: [{
-                data: venueCounts,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(54, 162, 235, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(54, 162, 235, 1)'
-                ],
-                borderWidth: 1
+                label: 'Forecasted Earnings (in millions)',
+                data: values,
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                borderWidth: 1,
+                fill: false
             }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
     });
 }
-
-function displayAggregateMetrics() {
-    const totalRevenue = data.reduce((acc, row) => acc + (row['Total Revenue'] || 0), 0);
-    const averageEventBreakdown = data.reduce((acc, row) => acc + (row['Event Breakdown'] || 0), 0) / data.length;
-    document.getElementById('aggregateMetrics').innerHTML = `
-        <strong>Total Revenue:</strong> $${totalRevenue.toFixed(2)}<br>
-        <strong>Average Event Breakdown:</strong> $${averageEventBreakdown.toFixed(2)}
-    `;
-}
-
-// D3.js Tooltip for extra information on hover
-const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-document.getElementById('visualization').onmousemove = function(event) {
-    const index = Math.floor(event.offsetX / 100);
-    if (data[index]) {
-        tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-        tooltip.html(`
-            Sponsors: ${data[index]['Sponsors']}<br>
-            Venue: ${data[index]['Venue']}<br>
-            Total Revenue: $${(data[index]['Total Revenue'] || 0).toLocaleString()}
-        `)
-        .style("left", (event.pageX + 5) + "px")
-        .style("top", (event.pageY - 28) + "px");
-    }
-};
-
-document.getElementById('visualization').onmouseout = function() {
-    tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-};
 
