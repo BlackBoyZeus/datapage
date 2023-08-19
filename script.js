e2fc7330f93e2cfb65b4c7efff8272f1e18ff457
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-08-19 11:38:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,177 @@
-// Global variable to store the data from the uploaded CSV
-let data = [];
+// Global variable for data storage
+let data;
 
-// Function to process the uploaded file and visualize it
-function processFile() {
+async function processFile() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
-    const reader = new FileReader();
 
-    reader.onload = function(event) {
-        const csv = event.target.result;
-        d3.csvParse(csv, function(d) {
-            // Assuming the CSV has columns 'label' and 'value' for simplicity
-            return {
-                label: d.label,
-                value: +d.value // Convert the value to a number
-            };
-        }).then(function(parsedData) {
-            data = parsedData;
-            visualizeData();
-        });
-    };
+    if (!file) {
+        updateStatus("No file selected.", "error");
+        return;
+    }
 
-    reader.readAsText(file);
+    try {
+        updateStatus("Reading file...");
+        const fileContent = await readFile(file);
+        
+        updateStatus("Processing CSV...");
+        data = csvToJSON(fileContent);
+        if (!data || data.length === 0) {
+            updateStatus("Error processing the CSV file.", "error");
+            return;
+        }
+
+        updateStatus("Visualizing data...");
+        displayBarChart();
+        displayPieChart();
+        displayAggregateMetrics();
+
+        updateStatus("Visualization complete!", "success");
+    } catch (error) {
+        updateStatus("An error occurred: " + error.message, "error");
+    }
 }
 
-// Function to visualize the uploaded data using a dynamic line chart
-function visualizeData() {
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = reject;
+        reader.readAsText(file);
+    });
+}
+
+function updateStatus(message, type = "info") {
+    const statusDiv = document.getElementById('status');
+    statusDiv.textContent = message;
+    statusDiv.className = type;
+}
+
+function csvToJSON(csv) {
+    const lines = csv.trim().split('\n');
+    const result = [];
+    const headers = lines[0].split(',');
+
+    for (let i = 1; i < lines.length; i++) {
+        const obj = {};
+        const currentLine = lines[i].split(',');
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentLine[j];
+        }
+        result.push(obj);
+    }
+    return result;
+}
+
+function displayBarChart() {
     const ctx = document.getElementById('visualization').getContext('2d');
+    const labels = data.map(row => row['Sponsors'] || 'Unknown');
+    const values = data.map(row => parseFloat(row['Event Breakdown'] || 0));
 
-    const labels = data.map(d => d.label);
-    const values = data.map(d => d.value);
-
-    const chart = new Chart(ctx, {
-        type: 'line',
+    new Chart(ctx, {
+        type: 'bar',
         data: {
             labels: labels,
             datasets: [{
-                label: 'Data',
+                label: 'Event Breakdown ($)',
                 data: values,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
-                fill: false
+                borderWidth: 1
             }]
         },
         options: {
-            animation: {
-                duration: 2000,
-                easing: 'easeInOutQuad'
-            },
+            responsive: true,
             scales: {
-                y: {
-                    beginAtZero: true
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            plugins: {
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'xy'
+                    },
+                    zoom: {
+                        enabled: true,
+                        mode: 'xy'
+                    }
                 }
             }
         }
     });
-
-    // Animate the chart data in
-    setTimeout(() => {
-        chart.update();
-    }, 500);
 }
+
+function displayPieChart() {
+    const ctxMetrics = document.getElementById('metrics').getContext('2d');
+    const venues = [...new Set(data.map(row => row['Venue']))];
+    const venueCounts = venues.map(venue => data.filter(row => row['Venue'] === venue).length);
+
+    new Chart(ctxMetrics, {
+        type: 'pie',
+        data: {
+            labels: venues,
+            datasets: [{
+                data: venueCounts,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
+
+function displayAggregateMetrics() {
+    const totalRevenue = data.reduce((acc, row) => acc + parseFloat(row['Total Revenue'] || 0), 0);
+    const averageEventBreakdown = data.reduce((acc, row) => acc + parseFloat(row['Event Breakdown'] || 0), 0) / data.length;
+
+    document.getElementById('aggregateMetrics').innerHTML = `
+        <strong>Total Revenue:</strong> $${totalRevenue.toFixed(2)}<br>
+        <strong>Average Event Breakdown:</strong> $${averageEventBreakdown.toFixed(2)}
+    `;
+}
+
+// D3.js Tooltip for extra information on hover
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+document.getElementById('visualization').onmousemove = function(event) {
+    const index = Math.floor(event.offsetX / 100);
+    if (data[index]) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html(`
+            Sponsors: ${data[index]['Sponsors']}<br>
+            Venue: ${data[index]['Venue']}<br>
+            Total Revenue: ${data[index]['Total Revenue']}
+        `)
+        .style("left", (event.pageX + 5) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    }
+};
+
+document.getElementById('visualization').onmouseout = function() {
+    tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+};
 
 // Function to run earnings forecast simulation and visualize it with random spikes
 function earnings_forecast_simulation_millions() {
